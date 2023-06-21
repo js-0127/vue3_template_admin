7fef7165dd1e27d79a893ddd90ef2c890c0ddd50
js-0127 <w20021125@qq.com>
2023-06-21 15:46:19 +0800

    feat: addBox

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,20 @@
-<script setup lang="ts">
-const log = () => {
-
-}
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  <div>
-
+  <div class="box">
   </div>
 </template>
- 
+
 <style lang="scss">
 html,
 #app {
   width: 100%;
   height: 100%;
+
+  .box {
+    width: 200px;
+    height: 200px;
+    background-color: cyan;
+  }
 }
 </style>
